25017ba6c4f1825fbc4748547d7c430db7541cee
Joshua Bollman <jsbollman@gmail.com>
2022-08-29 11:25:56 -0500

    Created Hello World App for Assignment 1.2, github setup.

---
+++ Module_1/HelloWorldApp/BollmanHelloWorld.java
@@ -0,0 +1,16 @@
+/*
+    Krasso, R., (2021). CIS 505 Intermediate Java Programming. Bellevue University, all
+        rights reserved.
+
+    W3Schools. (n.d.). Java tutorial. Retrieved August 29, 2022, from https://www.w3schools.com/java/ 
+    Modified by J. Bollman 2022
+ */
+public class BollmanHelloWorld {
+    public static void main(String[] args) {
+        String lFirstName = "Joshua"; // Instantiate the first name variable.
+        String lLastName = "Bollman"; // Instantiate the last name variable.
+
+        // Output a formatted string to the console using the first and last name variables.
+        System.out.printf("Hello World from %s %s", lFirstName, lLastName);
+    } // end of main
+} // end BollmanHelloWorld
